99adcb06a41cba688826c352de1f1c3f3d5a4fa5
Nilanjan <roynilanjan1@gmail.com>
2020-02-17 15:52:32 +0530

    App Configuration changes: Assignment 2

--- src/main/java/io.pivotal.pal.tracker/WelcomeController.java
+++ src/main/java/io.pivotal.pal.tracker/WelcomeController.java
@@ -1,13 +1,35 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
 
+    String hello;
+
+
+    public WelcomeController(@Value("${welcome.message}") String message){
+        hello=message;
+    }
+
     @GetMapping("/")
     public String sayHello(){
-        return  "hello";
+        return  hello;
     }
+/*
+    @GetMapping("/hello")
+    public String sayHelloWorld(){
+        return "hello-world";
+
+    }
+    @GetMapping("/test")
+    public String test(){
+        return "test";
+    }*/
+
+
+
+
 }
