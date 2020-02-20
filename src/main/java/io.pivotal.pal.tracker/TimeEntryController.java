0e7b2508b2ca878b8415dff025a87a67561743cb
Nilanjan <roynilanjan1@gmail.com>
2020-02-20 09:56:47 +0530

    Health Monitoring

--- src/main/java/io.pivotal.pal.tracker/TimeEntryController.java
+++ src/main/java/io.pivotal.pal.tracker/TimeEntryController.java
@@ -1,5 +1,8 @@
 package io.pivotal.pal.tracker;
 
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.DistributionSummary;
+import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,14 +16,20 @@ import java.util.Map;
 public class TimeEntryController {
 
     TimeEntryRepository timeEntryRepository;
+    private final DistributionSummary timeEntrySummary;
+    private final Counter actionCounter;
 
-    public TimeEntryController(TimeEntryRepository timeEntryRepository) {
+    public TimeEntryController(TimeEntryRepository timeEntryRepository, MeterRegistry meterRegistry) {
         this.timeEntryRepository = timeEntryRepository;
+        timeEntrySummary = meterRegistry.summary("timeEntry.summary");
+        actionCounter = meterRegistry.counter("timeEntry.actionCounter");
     }
 
     @PostMapping
     public ResponseEntity<TimeEntry> create(@RequestBody TimeEntry timeEntry){
            TimeEntry timeEntryObj = timeEntryRepository.create(timeEntry);
+            actionCounter.increment();
+            timeEntrySummary.record(timeEntryRepository.list().size());
            return new ResponseEntity<TimeEntry>(timeEntryObj, HttpStatus.CREATED);
         //return  null;
     }
@@ -29,6 +38,8 @@ public class TimeEntryController {
         TimeEntry timeEntryObj = timeEntryRepository.find(id);
         if(null == timeEntryObj){
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }else{
+            actionCounter.increment();
         }
         return new ResponseEntity<TimeEntry>(timeEntryObj, HttpStatus.OK);
     }
@@ -36,6 +47,7 @@ public class TimeEntryController {
     @GetMapping
     public ResponseEntity<List<TimeEntry>> list(){
         List<TimeEntry> timeEntryObjList = timeEntryRepository.list();
+        actionCounter.increment();
         return new ResponseEntity<List<TimeEntry>>(timeEntryObjList, HttpStatus.OK);
     }
 
@@ -45,6 +57,8 @@ public class TimeEntryController {
         TimeEntry timeEntryObj = timeEntryRepository.update(id, timeEntry);
         if(null == timeEntryObj){
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }else{
+            actionCounter.increment();
         }
         return new ResponseEntity<TimeEntry>(timeEntryObj, HttpStatus.OK);
     }
@@ -53,6 +67,8 @@ public class TimeEntryController {
     public ResponseEntity delete(@PathVariable Long id){
         //if()
         timeEntryRepository.delete(id);
+        actionCounter.increment();
+        timeEntrySummary.record(timeEntryRepository.list().size());
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
--- src/test/java/test/pivotal/pal/trackerapi/HealthApiTest.java
+++ src/test/java/test/pivotal/pal/trackerapi/HealthApiTest.java
@@ -29,7 +29,6 @@ public class HealthApiTest {
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
 
         DocumentContext healthJson = parse(response.getBody());
-
         assertThat(healthJson.read("$.status", String.class)).isEqualTo("UP");
         assertThat(healthJson.read("$.components.db.status", String.class)).isEqualTo("UP");
         assertThat(healthJson.read("$.components.diskSpace.status", String.class)).isEqualTo("UP");
